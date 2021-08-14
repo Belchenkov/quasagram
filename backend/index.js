1a48e787922254e47737c81fcf2945fc85be2a25
Aleksey Belchenkov <u608110@gmail.com>
2021-08-14 20:48:01 +0400

    Uploaded Image to fb

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const admin = require('firebase-admin');
 const inspect = require('util').inspect;
 const Busboy = require('busboy');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const UUID = require('uuid-v4');
 
 const app = express();
 
@@ -11,10 +15,12 @@ const app = express();
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "quasagram-6e55e.appspot.com"
 });
 
 const db = admin.firestore();
+const bucket = admin.storage().bucket();
 
 app.get('/posts',  (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
@@ -33,17 +39,18 @@ app.get('/posts',  (req, res) => {
 
 app.post('/posts',  (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
+
   const busboy = new Busboy({ headers: req.headers });
   const fields = {};
+  let fileData = {};
+  const uuid = UUID();
 
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
     console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-    file.on('data', function(data) {
-      console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-    });
-    file.on('end', function() {
-      console.log('File [' + fieldname + '] Finished');
-    });
+    const filePath = path.join(os.tmpdir(), filename);
+
+    file.pipe(fs.createWriteStream(filePath));
+    fileData = { filePath, mimetype };
   });
 
   busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
@@ -54,18 +61,36 @@ app.post('/posts',  (req, res) => {
   busboy.on('finish', function() {
     const { id, caption, location, date } = fields;
 
-    db.collection('posts')
-      .doc(id)
-      .set({
-        id,
-        caption,
-        location,
-        date: parseInt(date),
-        imageUrl: ''
-      });
-    return res.send('Done parsing form!');
-    //res.writeHead(303, { Connection: 'close', Location: '/' });
-    res.end();
+    bucket.upload(fileData.filePath, {
+      uploadType: 'media',
+      metadata: {
+        metadata: {
+          contentType: fileData.mimetype,
+          firebaseStorageDownloadTokens: uuid
+        }
+      }
+    },
+(err, uploadFile) => {
+        if (!err) {
+          createDocument(uploadFile);
+        }
+      }
+    );
+
+    function createDocument(file) {
+      db.collection('posts')
+        .doc(id)
+        .set({
+          id,
+          caption,
+          location,
+          date: parseInt(date),
+          imageUrl: `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${file.name}?alt=media&token=${uuid}`
+        })
+        .then(() => {
+          return res.send('Post added: ' + fields.id);
+        });
+    }
   });
 
   req.pipe(busboy);

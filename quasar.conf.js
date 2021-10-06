73ed503221b87cdf9a2f470176124feb1a8f4c6d
Алексей Бельченков <u608110@gmail.com>
2021-10-06 21:12:57 +0400

    Setup PWA & File Manifest

--- quasar.conf.js
+++ quasar.conf.js
@@ -119,13 +119,13 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `quasagram`,
+        name: `Quasagram - Share your experiences`,
         short_name: `quasagram`,
-        description: `PWA Quasar App`,
+        description: `A simple instagram clone - Made with Quasar`,
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#212121',
         icons: [
           {
             src: 'icons/icon-128x128.png',
